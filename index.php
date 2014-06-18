f17adf51ce9643719cf4b31282d1478eb732bbc8
gergalyb <gergalyb@gmail.com>
2014-06-18 11:40:50 +0200

    base files created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>index.php</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>test</h1>
+		<a href="login.php"><h1>login.php</h1></a>
+	</body>
+</html>
